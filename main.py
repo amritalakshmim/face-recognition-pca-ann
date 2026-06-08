9fca9a8f8ceb5f9f98b0db325e82d7422de88f05
Amritalakshmi Manojkumar <amritalakshmim@gmail.com>
2026-06-08 21:11:24 +0530

    Implement PCA eigen decomposition and eigenface generation

--- main.py
+++ main.py
@@ -32,3 +32,41 @@ print(mean_face.shape)
 print("\nMean Centered Face Shape:")
 print(mean_centered_faces.shape)
 
+
+covariance_matrix = (
+    pca.compute_covariance_matrix()
+)
+
+print("\nCovariance Matrix Shape:")
+print(covariance_matrix.shape)
+
+eigenvalues, eigenvectors = (
+    pca.compute_eigen_components(
+        covariance_matrix
+    )
+)
+
+print("\nEigenvalues Shape:")
+print(eigenvalues.shape)
+
+print("\nEigenvectors Shape:")
+print(eigenvectors.shape)
+
+feature_vectors = (
+    pca.select_top_k_components(
+        eigenvalues,
+        eigenvectors
+    )
+)
+
+print("\nTop K Feature Vectors Shape:")
+print(feature_vectors.shape)
+
+eigenfaces = (
+    pca.generate_eigenfaces(
+        feature_vectors
+    )
+)
+
+print("\nEigenfaces Shape:")
+print(eigenfaces.shape)
--- src/pca.py
+++ src/pca.py
@@ -32,3 +32,80 @@ class PCAFaceRecognizer:
         )
 
         return self.mean_centered_faces
+    
+    def compute_covariance_matrix(
+            self
+    ):
+        
+        covariance_matrix = np.dot(
+            self.mean_centered_faces.T,
+            self.mean_centered_faces
+        )
+
+        return covariance_matrix
+    
+    def compute_eigen_components(
+            self,
+            covariance_matrix
+    ):
+        
+        eigenvalues, eigenvectors = np.linalg.eig(
+            covariance_matrix
+        )
+
+        return eigenvalues, eigenvectors
+    
+    def select_top_k_components(
+            self,
+            eigenvalues,
+            eigenvectors
+    ):
+
+        sorted_indices = np.argsort(
+            eigenvalues
+        )[::-1]
+
+        sorted_eigenvectors = (
+            eigenvectors[:,sorted_indices]
+        )
+
+        selected_vectors = (
+            sorted_eigenvectors[:, :self.k]
+        )
+
+        feature_vectors = np.dot(
+            self.mean_centered_faces,
+            selected_vectors
+        )
+
+        return feature_vectors
+    
+    def generate_eigenfaces(
+            self,
+            feature_vectors
+    ):
+        eigenfaces = []
+
+        for i in range(
+            feature_vectors.shape[1]
+        ):
+            eigenface = (
+                feature_vectors[:, i]
+            )
+
+            normalized_face = (
+                eigenface / 
+                np.linalg.norm(
+                    eigenface
+                )
+            )
+
+            eigenfaces.append(
+                normalized_face
+            )
+
+        eigenfaces = np.array(
+            eigenfaces
+        ).T
+
+        return eigenfaces
