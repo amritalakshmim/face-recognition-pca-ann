a84ba642556948f88c1a5406cecba9fe891def17
Amritalakshmi Manojkumar <amritalakshmim@gmail.com>
2026-06-11 21:36:24 +0530

    Documented evaluate module

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 
 
@@ -5,11 +6,37 @@ def plot_accuracy_vs_k(
     k_values,
     accuracies
 ):
+    """
+    Plot model accuracy
+    against different
+    PCA k values.
+
+    This function visualizes
+    how the number of PCA
+    components affects the
+    face recognition accuracy.
+
+    The graph helps identify
+    the optimal k value
+    for best performance.
+
+    Parameters
+    ----------
+    k_values : list
+        List of PCA
+        component values.
+
+    accuracies : list
+        Accuracy percentages
+        corresponding
+        to each k value.
+    """
 
     plt.figure(
         figsize=(8, 5)
     )
 
+    # Plot accuracy curve
     plt.plot(
         k_values,
         accuracies,
@@ -30,8 +57,17 @@ def plot_accuracy_vs_k(
 
     plt.grid(True)
 
+    # Create output folder
+    # if it does not exist
+    os.makedirs(
+        "outputs/graphs",
+        exist_ok=True
+    )
+
+    # Save graph
     plt.savefig(
-        "outputs/graphs/accuracy_vs_k.png"
+        "outputs/graphs/"
+        "accuracy_vs_k.png"
     )
 
     plt.show()
